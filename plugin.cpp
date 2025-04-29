5ded1a3e48bcc26dd24d7aec0478ee47e38bbff5
Matt Schwartz <68516435+mzschwartz5@users.noreply.github.com>
2025-04-29 18:18:52 -0400

    GUI (#10)
    * Exposes UI display grid for voxelization
    
    * Adds voxelizer command to custom shelf
    
    * Set up of voxel simulation node (WIP)
    
    * Fixes UI template for sim node by loading as resource file
    
    * A LOT of UI-related changes
    
    Adds a new voxelizer menu on click plugin from shelf. Wires up menu values
    to command arguments. Packages new menu as resource file.
    
    * More robust selection process and fallbacks for GUI
    
    * Fix for local to world space for bbox
    
    * Condense MEL strings into script and centralizes resource file loading as util call
    
    * Inline resource loader

--- plugin.cpp
+++ plugin.cpp
@@ -1,5 +1,9 @@
 #include "plugin.h"
 #include "glm/glm.hpp"
+#include <maya/MCommandResult.h>
+#include "voxelsimulationnode.h"
+#include <maya/MFnMessageAttribute.h>
+#include <windows.h>
 
 // define EXPORT for exporting dll functions
 #define EXPORT __declspec(dllexport)
@@ -37,28 +41,58 @@ void plugin::simulate(void* clientData) {
 	MGlobal::executeCommand("refresh");
 }
 
+
+MSyntax plugin::syntax()
+{
+	MSyntax syntax;
+	syntax.addFlag("-px", "-positionX", MSyntax::kDouble);
+	syntax.addFlag("-py", "-positionY", MSyntax::kDouble);
+	syntax.addFlag("-pz", "-positionZ", MSyntax::kDouble);
+	syntax.addFlag("-s", "-scale", MSyntax::kDouble);
+	syntax.addFlag("-v", "-voxelsPerEdge", MSyntax::kLong);
+	syntax.addFlag("-n", "-gridDisplayName", MSyntax::kString);
+	return syntax;
+}
+
 // Plugin doIt function
 MStatus plugin::doIt(const MArgList& argList)
 {
-	MGlobal::displayInfo("VoxelDestroyer command executed.");
-
 	MStatus status;
-	float gridEdgeLength = 3.0f;
-	float numVoxelsPerEdge = 5.0f;
-	float voxelSize = gridEdgeLength / numVoxelsPerEdge;
+
+	PluginArgs pluginArgs = parsePluginArgs(argList);
+	MDagPath selectedMeshDagPath = getSelectedObject(pluginArgs.position, pluginArgs.scale);
+	// Fall back to finding the closest object to the voxel grid if nothing is selected
+	if (selectedMeshDagPath == MDagPath() || status != MS::kSuccess) {
+		selectedMeshDagPath = findClosestObjectToVoxelGrid(pluginArgs.position, pluginArgs.scale, pluginArgs.gridDisplayName);
+		if (selectedMeshDagPath == MDagPath()) {
+			MGlobal::displayError("No mesh found to voxelize.");
+			return MS::kFailure;
+		}
+
+		MSelectionList selectionList;
+		selectionList.add(selectedMeshDagPath);
+		MGlobal::setActiveSelectionList(selectionList);
+	}
+
+	MGlobal::displayInfo("Selected mesh: " + selectedMeshDagPath.fullPathName());
+
+	float voxelSize = static_cast<float>(pluginArgs.scale / pluginArgs.voxelsPerEdge);
 	Voxels voxels = voxelizer.voxelizeSelectedMesh(
-		gridEdgeLength, //size of the grid
-		voxelSize, // voxel size
-		MPoint(0.0f, 3.0f, 0.0f), // grid center
+		static_cast<float>(pluginArgs.scale),
+		voxelSize,
+		pluginArgs.position,
+		selectedMeshDagPath,
 		plugin::voxelizedMeshDagPath,
 		status
 	);
 	MFnMesh voxelMeshFn(plugin::voxelizedMeshDagPath);
+
+	plugin::createVoxelSimulationNode();
 	
 	MGlobal::displayInfo("Mesh voxelized. Dag path: " + plugin::voxelizedMeshDagPath.fullPathName());
 
 	// TODO: With the current set up, this wouldn't allow us to support voxelizing and simulating multiple meshes at once.
-	plugin::pbdSimulator = PBD(voxels, voxelSize, gridEdgeLength);
+	plugin::pbdSimulator = PBD(voxels, voxelSize, pluginArgs.scale);
 	MGlobal::displayInfo("PBD particles initialized.");
 
 	// TODO: handle if doIt is called repeatedly... this will just create new buffers but not free old ones?
@@ -93,6 +127,225 @@ MStatus plugin::doIt(const MArgList& argList)
 	return status;
 }
 
+PluginArgs plugin::parsePluginArgs(const MArgList& args) {
+	PluginArgs pluginArgs;
+	MStatus status;
+
+	MArgDatabase argData(syntax(), args, &status);
+	if (status != MS::kSuccess) {
+		MGlobal::displayError("Failed to parse arguments: " + status.errorString());
+		return pluginArgs;
+	}
+	
+	// Voxel grid center position
+	if (argData.isFlagSet("-px")) {
+		status = argData.getFlagArgument("-px", 0, pluginArgs.position.x);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get position X: " + status.errorString());
+		}
+	}
+	if (argData.isFlagSet("-py")) {
+		status = argData.getFlagArgument("-py", 0, pluginArgs.position.y);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get position Y: " + status.errorString());
+		}
+	}
+	if (argData.isFlagSet("-pz")) {
+		status = argData.getFlagArgument("-pz", 0, pluginArgs.position.z);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get position Z: " + status.errorString());
+		}
+	}
+
+	// Voxel grid edge length (scale)
+	if (argData.isFlagSet("-s")) {
+		status = argData.getFlagArgument("-s", 0, pluginArgs.scale);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get scale: " + status.errorString());
+		}
+	}
+	
+	// Number of voxels per edge
+	if (argData.isFlagSet("-v")) {
+		status = argData.getFlagArgument("-v", 0, pluginArgs.voxelsPerEdge);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get voxels per edge: " + status.errorString());
+		}
+	}
+
+	if (argData.isFlagSet("-n")) {
+		status = argData.getFlagArgument("-n", 0, pluginArgs.gridDisplayName);
+		if (status != MS::kSuccess) {
+			MGlobal::displayError("Failed to get grid display name: " + status.errorString());
+		}
+	}
+
+	return pluginArgs;
+}
+
+MDagPath plugin::getSelectedObject(const MPoint& voxelGridCenter, double voxelGridSize) {
+	MStatus status;
+    // Get the current selection
+    MSelectionList selection;
+    MGlobal::getActiveSelectionList(selection);
+
+    // Check if the selection is empty
+    if (selection.isEmpty()) {
+        MGlobal::displayWarning("No mesh selected.");
+        status = MS::kFailure;
+        return MDagPath();
+    }
+
+    // Get the first selected item and ensure it's a mesh
+    MDagPath activeMeshDagPath;
+    status = selection.getDagPath(0, activeMeshDagPath);
+    if (status != MS::kSuccess || !activeMeshDagPath.hasFn(MFn::kMesh)) {
+        MGlobal::displayError("The selected item is not a mesh.");
+        status = MS::kFailure;
+        return MDagPath();
+    }
+
+	MFnMesh meshFn(activeMeshDagPath);
+	MBoundingBox boundingBox = meshFn.boundingBox();
+	MPoint boundingBoxCenter = boundingBox.center();
+	MMatrix worldMatrix = activeMeshDagPath.inclusiveMatrix();
+	boundingBox.transformUsing(worldMatrix);
+
+	if (!isBoundingBoxOverlappingVoxelGrid(boundingBox, voxelGridCenter, voxelGridSize)) {
+		MGlobal::displayError("The selected mesh is not within the voxel grid.");
+		status = MS::kFailure;
+		return MDagPath();
+	}
+
+    return activeMeshDagPath;
+}
+
+MDagPath plugin::findClosestObjectToVoxelGrid(const MPoint& voxelGridCenter, double voxelGridSize, MString gridDisplayName) {
+    MDagPath closestDagPath;
+    double closestDistance = std::numeric_limits<double>::max();
+
+    // Iterate through all DAG nodes in the scene
+    MItDag dagIterator(MItDag::kDepthFirst, MFn::kTransform);
+    for (; !dagIterator.isDone(); dagIterator.next()) {
+        MDagPath currentDagPath;
+        dagIterator.getPath(currentDagPath);
+
+        if (currentDagPath.node().hasFn(MFn::kTransform)) {
+			// Skip the grid display object by comparing the transform node name
+			if (currentDagPath.partialPathName() == gridDisplayName) continue;
+
+            // Get the shape node under the transform
+            MDagPath shapeDagPath = currentDagPath;
+            if (shapeDagPath.extendToShape() != MS::kSuccess || !shapeDagPath.node().hasFn(MFn::kMesh)) continue;
+
+            MFnMesh meshFn(shapeDagPath);
+            MBoundingBox boundingBox = meshFn.boundingBox();
+            MPoint boundingBoxCenter = boundingBox.center();
+			MMatrix worldMatrix = shapeDagPath.inclusiveMatrix();
+			boundingBox.transformUsing(worldMatrix);
+            double distance = (MVector(boundingBoxCenter) - MVector(voxelGridCenter)).length();
+
+            if (!isBoundingBoxOverlappingVoxelGrid(boundingBox, voxelGridCenter, voxelGridSize)) continue;
+
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestDagPath = currentDagPath; // Keep the transform node path
+            }
+        }
+    }
+
+    if (!closestDagPath.isValid()) {
+        MGlobal::displayWarning("No objects with meshes found in the scene.");
+    }
+
+    return closestDagPath;
+}
+
+bool plugin::isBoundingBoxOverlappingVoxelGrid(const MBoundingBox& objectBoundingBox, const MPoint& voxelGridCenter, double voxelGridSize) {
+	return (voxelGridCenter.x - voxelGridSize / 2.0 <= objectBoundingBox.max().x &&
+			voxelGridCenter.x + voxelGridSize / 2.0 >= objectBoundingBox.min().x &&
+			voxelGridCenter.y - voxelGridSize / 2.0 <= objectBoundingBox.max().y &&
+			voxelGridCenter.y + voxelGridSize / 2.0 >= objectBoundingBox.min().y &&
+			voxelGridCenter.z - voxelGridSize / 2.0 <= objectBoundingBox.max().z &&
+			voxelGridCenter.z + voxelGridSize / 2.0 >= objectBoundingBox.min().z
+	);
+}
+
+void plugin::createVoxelSimulationNode() {
+    MStatus status;
+
+    // Create the VoxelSimulationNode
+    MFnDependencyNode depNodeFn;
+    MObject voxelSimNode = depNodeFn.create(VoxelSimulationNode::id, &status);
+    if (status != MS::kSuccess) {
+        MGlobal::displayError("Failed to create VoxelSimulationNode: " + status.errorString());
+        return;
+    }
+
+    // Add a message attribute to the voxelized mesh's transform node
+    MFnDagNode dagNode(plugin::voxelizedMeshDagPath.transform(&status));
+    if (status != MS::kSuccess) {
+        MGlobal::displayError("Failed to get transform node: " + status.errorString());
+        return;
+    }
+
+    MFnMessageAttribute msgAttrFn;
+    MObject messageAttr;
+    if (!dagNode.hasAttribute("voxelSimulationNode")) {
+        messageAttr = msgAttrFn.create("voxelSimulationNode", "vsn", &status);
+        if (status != MS::kSuccess) {
+            MGlobal::displayError("Failed to create message attribute: " + status.errorString());
+            return;
+        }
+        dagNode.addAttribute(messageAttr);
+    } else {
+        messageAttr = dagNode.attribute("voxelSimulationNode", &status);
+    }
+
+    // Connect the VoxelSimulationNode to the transform node using the message attribute
+	MPlug meshPlug = dagNode.findPlug(messageAttr, false, &status);
+    MPlug simNodePlug = depNodeFn.findPlug("message", false, &status); // The default "message" attribute of the node
+    if (status == MS::kSuccess) {
+        MDGModifier dgModifier;
+        dgModifier.connect(simNodePlug, meshPlug);
+        dgModifier.doIt();
+    }
+
+}
+
+void plugin::loadVoxelSimulationNodeEditorTemplate() {
+	void* data = nullptr;
+	DWORD size = Utils::loadResourceFile(MhInstPlugin, IDR_MEL1, L"MEL", &data);
+	if (size == 0) {
+		MGlobal::displayError("Failed to load Voxelization editor template resource.");
+		return;
+	}
+
+    MString melScript(static_cast<char*>(data), size);
+
+    // Execute the MEL script to load the UI for the VoxelSimulationNode
+    MStatus status = MGlobal::executeCommand(melScript);
+    if (status != MS::kSuccess) {
+        MGlobal::displayError("Failed to execute AETEMPLATE MEL script: " + status.errorString());
+    }
+}
+
+void plugin::loadVoxelizerMenu() {
+	void* data = nullptr;
+	DWORD size = Utils::loadResourceFile(MhInstPlugin, IDR_MEL2, L"MEL", &data);
+	if (size == 0) {
+		MGlobal::displayError("Failed to load Voxelizer menu resource.");
+		return;
+	}
+
+	MString melScript(static_cast<char*>(data), size);
+
+	// Execute the MEL script to load the Voxelizer menu into memory
+	MStatus status = MGlobal::executeCommand(melScript);
+	if (status != MS::kSuccess) {
+		MGlobal::displayError("Failed to execute Voxelizer menu MEL script: " + status.errorString());
+	}
+}
 
 // Initialize Maya Plugin upon loading
 EXPORT MStatus initializePlugin(MObject obj)
@@ -112,7 +365,19 @@ EXPORT MStatus initializePlugin(MObject obj)
 	// Initialize DirectX
 	// MhInstPlugin is a global variable defined in the MfnPlugin.h file
 	DirectX::initialize(MhInstPlugin);
-	
+
+	plugin::loadVoxelSimulationNodeEditorTemplate();
+	plugin::loadVoxelizerMenu();
+
+	MGlobal::executeCommand("VoxelizerMenu_addToShelf");
+
+	// Register the VoxelSimulationNode
+	status = plugin.registerNode("VoxelSimulationNode", VoxelSimulationNode::id, VoxelSimulationNode::creator, VoxelSimulationNode::initialize);
+	if (!status) {
+		MGlobal::displayError("Failed to register VoxelSimulationNode");
+		return status;
+	}
+
 	return status;
 }
 
@@ -127,6 +392,13 @@ EXPORT MStatus uninitializePlugin(MObject obj)
 
 	// Deregister the callback
 	MEventMessage::removeCallback(plugin::getCallbackId());
+
+	status = plugin.deregisterNode(VoxelSimulationNode::id);
+	if (!status) {
+		MGlobal::displayError("Failed to deregister VoxelSimulationNode");
+	}
+
+	MGlobal::executeCommand("VoxelizerMenu_removeFromShelf");
 
 	return status;
 }
--- plugin.h
+++ plugin.h
@@ -2,6 +2,9 @@
 #include <maya/MArgList.h>
 #include <maya/MObject.h>
 #include <maya/MGlobal.h>
+#include <maya/MSyntax.h>
+#include <maya/MArgList.h>
+#include <maya/MArgDatabase.h>
 #include <maya/MPxCommand.h>
 #include <maya/MFnPlugin.h>
 #include <maya/MEventMessage.h>
@@ -9,6 +12,12 @@
 #include <maya/MPointArray.h>
 #include <maya/MDagPath.h>
 #include <memory>
+#include <maya/MFnDagNode.h>
+#include <maya/MFnTransform.h>
+#include <maya/MVector.h>
+#include <maya/MItDag.h>
+#include "utils.h"
+#include "pbd.h"
 #include <vector>
 #include "pbd.h"
 #include "voxelizer.h"
@@ -18,6 +27,13 @@
 #include "directx/compute/transformverticescompute.h"
 #include "directx/compute/bindverticescompute.h"
 
+struct PluginArgs {
+	MPoint position{ 0.0f, 0.0f, 0.0f };
+	double scale{ 1.0f };
+	int voxelsPerEdge{ 10 };
+	MString gridDisplayName{ "VoxelGridDisplay" };
+};
+
 // Making most functions and members static so we can bind methods to the timeChanged event
 // and access data during the special standalone intialize and uninitialize functions
 class plugin : public MPxCommand
@@ -26,10 +42,22 @@ public:
 	plugin() {};
 	// Called when the command ("VoxelDestroyer") is executed in Maya
 	virtual MStatus doIt(const MArgList& args);
+	PluginArgs parsePluginArgs(const MArgList& args);
 	// Called when the command is registered in Maya
 	static void* creator();
+	static MSyntax syntax();
 	// A callback bound to the timeChanged event (e.g. moving the animation slider)
 	static void simulate(void* clientData);
+	
+	// Compare the center of an object's bounding box to the center of the voxel grid
+	// to determine the closest object to the voxel grid (used as a fallback if nothing selected)
+	MDagPath findClosestObjectToVoxelGrid(const MPoint& voxelGridCenter, double voxelGridSize, MString gridDisplayName);
+	MDagPath getSelectedObject(const MPoint& voxelGridCenter, double voxelGridSize);
+	bool isBoundingBoxOverlappingVoxelGrid(const MBoundingBox& bbox, const MPoint& voxelGridCenter, double voxelGridSize);
+
+	static void createVoxelSimulationNode();
+	static void loadVoxelSimulationNodeEditorTemplate();
+	static void loadVoxelizerMenu();
 
 	static MCallbackId getCallbackId() { return plugin::callbackId; }
 	static MStatus setCallbackId(MCallbackId id) { 
@@ -50,6 +78,4 @@ private:
 	static int transformVerticesNumWorkgroups;
 	static std::unique_ptr<TransformVerticesCompute> transformVerticesCompute;
 	static std::unique_ptr<BindVerticesCompute> bindVerticesCompute;
-
-
 };
