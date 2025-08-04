97984905b6dea75923ad085321f2a678bf067539
Matt Schwartz <mzschwartz5@gmail.com>
2025-08-04 02:10:41 -0400

    Makes PBD sim into an MpxNode
    First step towards multi-mesh capability. Can instantiate multiple
    PBD nodes now - though they won't work simultaneously yet.
    
    Other things that don't work yet:
    - GPU override is still static
    - The serialization of the PBD node data to file appears to not work yet.
    - I gutted the VoxelSimulationNode class because it had direct pointers to PBD,
    and assumed PBD is static / singleton. The sim node logic will probably be
    absorbed into the PBD node anyway in the future.

--- pbd.h
+++ pbd.h
@@ -20,6 +20,7 @@
 #include "directx/compute/solvecollisionscompute.h"
 
 #include <maya/MGlobal.h>
+#include <maya/MPxNode.h>
 #include <maya/MSelectionList.h>
 #include <maya/MDagPath.h>
 #include <maya/MFnDagNode.h>
@@ -30,6 +31,10 @@
 #include <maya/MItDag.h>
 #include <maya/MFnMesh.h>
 #include <maya/MMatrix.h>
+#include <maya/MNodeMessage.h>
+#include <maya/MDataBlock.h>
+#include <maya/MCallbackIdArray.h>
+
 
 using glm::vec3;
 using glm::vec4;
@@ -42,49 +47,67 @@ struct Particles
     int numParticles{ 0 };
 };
 
-class PBD
+class PBD : public MPxNode
 {
 public:
-    PBD();
-    ~PBD();
-    void initialize(const Voxels& voxels, float voxelSize, const MDagPath& meshDagPath);
-    void simulateStep();
+    static MTypeId id;
+    static MString pbdNodeName;
+    
+    PBD() = default;
+    ~PBD() override;
+    // Functions for Maya to create and initialize the node
+    static void* creator() { return new PBD(); }
+    static MStatus initialize();
+    static void createPBDNode(Voxels&& voxels);
+    void postConstructor() override;
+    MStatus compute(const MPlug& plug, MDataBlock& dataBlock) override;
+    MPxNode::SchedulingType schedulingType() const override {
+        return MPxNode::kUntrusted; // Compute dispatches must be serial (at least for now - might be able to create deferred DX11 contexts)
+    }
+    static void onVoxelDataSet(MNodeMessage::AttributeMessage msg, MPlug& plug, MPlug& otherPlug, void* clientData);
 
-	void setRelaxation(float relaxation) { RELAXATION = relaxation; }
-	void setBeta(float beta) { BETA = beta; }
-	void setFTFRelaxation(float relaxation) { FTF_RELAXATION = relaxation; }
-	void setFTFBeta(float beta) { FTF_BETA = beta; }
-	void setGravityStrength(float strength) { GRAVITY_STRENGTH = strength; }
+    std::array<std::vector<FaceConstraint>, 3> constructFaceToFaceConstraints(
+        const Voxels& voxels, 
+        float xTension, float xCompression,
+        float yTension, float yCompression,
+        float zTension, float zCompression
+    );
 
-    void updateVGSInfo() {
-        vgsCompute->updateConstantBuffer({
-            RELAXATION, 
-            BETA, 
-            PARTICLE_RADIUS, 
-            VOXEL_REST_VOLUME, 
-            3.0,            // iter ocunt
-            FTF_RELAXATION, 
-            FTF_BETA, 
-            particles.numParticles / 8
-        });
+    Particles createParticles(const Voxels& voxels);
+
+    void createComputeShaders(
+        const Voxels& voxels, 
+        const Particles& particles,
+        const std::array<std::vector<FaceConstraint>, 3>& faceConstraints
+    );
+
+    void setRadiusAndVolumeFromLength(float edge_length) {
+        PARTICLE_RADIUS = edge_length * 0.25f;
+        VOXEL_REST_VOLUME = 8.0f * PARTICLE_RADIUS * PARTICLE_RADIUS * PARTICLE_RADIUS;
     }
 
-    void updateSimInfo() {
-        preVGSCompute->updateSimConstants({GRAVITY_STRENGTH, GROUND_COLLISION_ENABLED, TIMESTEP, particles.numParticles});
-    }
+    void subscribeToDragStateChange();
 
-    bool isInitialized() const { return initialized; }
+    void setInitialized(bool initialized) {
+        this->initialized = initialized;
+    }
     
 private:
-    Particles particles;
-    std::array<std::vector<FaceConstraint>, 3> faceConstraints; //x = 0, y = 1, z = 2
+    // Attributes
+    // Inputs
+    static MObject aTime;
+    static MObject aVoxelData;
+    // Output
+    static MObject aTrigger;
+
     int substeps = 10;
     float timeStep;
-    MDagPath meshDagPath;
+    int numParticles;
 
-    glm::vec4 simInfo;
     bool initialized = false;
     bool isDragging = false;
+
+    MCallbackIdArray callbackIds;
 
     // Event subscriptions
     std::function<void()> unsubscribeFromDragStateChange;
@@ -102,15 +125,6 @@ private:
     
     void simulateSubstep();
 
-    void constructFaceToFaceConstraints(const Voxels& voxels, 
-        float xTension, float xCompression,
-        float yTension, float yCompression,
-        float zTension, float zCompression);
-
-    void createParticles(const Voxels& voxels);
-
-    void setSimValuesFromUI();
-
     float BETA{ 0.0f };
     float PARTICLE_RADIUS{ 0.25f };
 
@@ -126,11 +140,4 @@ private:
 	float GROUND_COLLISION_ENABLED{ 1.f };
 	float GROUND_COLLISION_Y{ 0.f };
     float TIMESTEP{ 0.00166666666666667f };
-
-    void setRadiusAndVolumeFromLength(float edge_length) {
-        PARTICLE_RADIUS = edge_length * 0.25f;
-        VOXEL_REST_VOLUME = 8.0f * PARTICLE_RADIUS * PARTICLE_RADIUS * PARTICLE_RADIUS;
-    }
-
-    void addFaceConstraint(FaceConstraint constraint, int axis) { faceConstraints[axis].push_back(constraint); };
 };
