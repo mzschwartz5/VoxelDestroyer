0619d781f6cb0e8f4c8d77d66c8fb23efe2b3408
Matt Schwartz <mzschwartz5@gmail.com>
2025-09-05 00:28:45 -0400

    Adds subscene override to replicate mesh as custom shape

---
+++ custommayaconstructs/geometryoverride/voxelsubsceneoverride.h
@@ -0,0 +1,241 @@
+#pragma once
+
+#include <maya/MPxSubSceneOverride.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnMesh.h>
+#include <maya/MShaderManager.h>
+#include "voxelshape.h"
+#include <maya/MGeometryRequirements.h>
+#include <maya/MGeometryExtractor.h>
+#include <memory>
+using namespace MHWRender;
+using std::unique_ptr;
+using std::make_unique;
+
+struct RenderItemInfo {
+    MIndexBufferDescriptor indexDesc;
+    MShaderInstance* shaderInstance;
+    MString renderItemName;
+};
+
+class VoxelSubSceneOverride : public MPxSubSceneOverride {
+public:
+    inline static MString drawDbClassification = "drawdb/subscene/voxelSubsceneOverride";
+    inline static MString drawRegistrantId = "VoxelSubSceneOverridePlugin";
+
+    static MPxSubSceneOverride* creator(const MObject& obj) 
+    {
+        return new VoxelSubSceneOverride(obj);
+    }
+
+    ~VoxelSubSceneOverride() override = default;
+    
+    DrawAPI supportedDrawAPIs() const override {
+        return kDirectX11;
+    }
+
+    bool requiresUpdate(
+        const MSubSceneContainer& container,
+        const MFrameContext& frameContext) const override
+    {
+        return (container.count() == 0);
+    }
+
+    MShaderInstance* getVertexBufferDescriptorsForShader(const MObject& shaderNode, const MDagPath& geomDagPath, MVertexBufferDescriptorList& vertexBufferDescriptors) {
+        MRenderer* renderer = MRenderer::theRenderer();
+        if (!renderer) return nullptr;
+
+        const MShaderManager* shaderManager = renderer->getShaderManager();
+        if (!shaderManager) return nullptr;
+
+        MShaderInstance* shaderInstance = shaderManager->getShaderFromNode(shaderNode, geomDagPath);
+        if (!shaderInstance) return nullptr;
+
+        shaderInstance->requiredVertexBuffers(vertexBufferDescriptors);
+
+        return shaderInstance;
+    }
+
+    MObject getShaderNodeFromShadingSet(const MObject& shadingSet) {
+        MFnDependencyNode fnShadingSet(shadingSet);
+        MPlug shaderPlug = fnShadingSet.findPlug("surfaceShader", true);
+        MPlugArray conns;
+        if (shaderPlug.isNull() || !shaderPlug.connectedTo(conns, true, false) || conns.length() == 0) return MObject::kNullObj;
+        return conns[0].node(); // API returns a plug array but there can only be one shader connected.
+    }
+
+    MObjectArray getShadingSetFaceComponents(const MObjectArray& shadingSets, const MIntArray& faceIdxToShader) {
+        MObjectArray shadingSetFaceComponents;
+        shadingSetFaceComponents.setLength(shadingSets.length());
+        MFnSingleIndexedComponent fnFaceComponent;
+
+        for (uint i = 0; i < shadingSets.length(); ++i) {
+            shadingSetFaceComponents[i] = fnFaceComponent.create(MFn::kMeshPolygonComponent);
+        }
+
+        for (uint i = 0; i < faceIdxToShader.length(); ++i) {
+            int shadingSetIdx = faceIdxToShader[i];
+            if (shadingSetIdx < 0 || shadingSetIdx >= (int)shadingSets.length()) continue;
+            
+            fnFaceComponent.setObject(shadingSetFaceComponents[shadingSetIdx]);
+            fnFaceComponent.addElement(i);
+        }
+
+        return shadingSetFaceComponents;
+    }
+
+    void buildGeometryRequirements(
+        const MObjectArray& shadingSets, 
+        const MObjectArray& shadingSetFaceComponents,
+        const MDagPath& originalGeomPath,
+        MGeometryRequirements& geomReqs,
+        std::vector<RenderItemInfo>& renderItemInfos
+    ) {
+        MFnSingleIndexedComponent fnFaceComponent;
+        MFnMesh originalMeshFn(originalGeomPath.node());
+
+        MString uvSet;
+        originalMeshFn.getCurrentUVSetName(uvSet);
+        const bool haveUVs = (uvSet.length() > 0) && (originalMeshFn.numUVs(uvSet) > 0);
+        
+        std::unordered_set<MGeometry::Semantic> existingVBRequirements;
+        for (uint i = 0; i < shadingSets.length(); ++i) {
+            fnFaceComponent.setObject(shadingSetFaceComponents[i]);
+            if (fnFaceComponent.elementCount() == 0) continue;
+
+            MObject shaderNode = getShaderNodeFromShadingSet(shadingSets[i]);
+            if (shaderNode.isNull()) continue;
+
+            MVertexBufferDescriptorList vbDescList;
+            MShaderInstance* shaderInstance = getVertexBufferDescriptorsForShader(shaderNode, originalGeomPath, vbDescList);
+            if (!shaderInstance) continue;
+
+            for (int j = 0; j < vbDescList.length(); ++j) {
+                MVertexBufferDescriptor vbDesc;
+                if (!vbDescList.getDescriptor(j, vbDesc)) continue;
+                
+                if (existingVBRequirements.find(vbDesc.semantic()) != existingVBRequirements.end()) continue;
+                existingVBRequirements.insert(vbDesc.semantic());
+
+                if (vbDesc.semantic() == MGeometry::kTexture && !haveUVs) continue;
+                geomReqs.addVertexRequirement(vbDesc);
+            }
+
+            MIndexBufferDescriptor indexDesc(
+                MIndexBufferDescriptor::kTriangle,
+                MString(), // unused for kTriangle
+                MGeometry::kTriangles,
+                0, // unused for kTriangle
+                shadingSetFaceComponents[i]
+            );
+
+            geomReqs.addIndexingRequirement(indexDesc);
+
+            renderItemInfos.push_back({indexDesc, shaderInstance, 
+                "voxelRenderItem_" + MFnDependencyNode(shadingSets[i]).name()});
+        }
+
+    }
+
+    void createVertexBuffer(const MVertexBufferDescriptor& vbDesc, const MGeometryExtractor& extractor, uint vertexCount, MVertexBufferArray& vertexBufferArray) {
+        auto vertexBuffer = make_unique<MVertexBuffer>(vbDesc);
+        void* data = vertexBuffer->acquire(vertexCount, true);
+        extractor.populateVertexBuffer(data, vertexCount, vbDesc);
+        vertexBuffer->commit(data);
+        vertexBufferArray.addBuffer(vbDesc.name(), vertexBuffer.get());
+        vertexBuffers.push_back(std::move(vertexBuffer));
+    }
+
+    MIndexBuffer* createIndexBuffer(const RenderItemInfo& itemInfo, const MGeometryExtractor& extractor) {
+        unsigned int numTriangles = extractor.primitiveCount(itemInfo.indexDesc);
+        if (numTriangles == 0) return nullptr;
+
+        auto indexBuffer = make_unique<MIndexBuffer>(MGeometry::kUnsignedInt32);
+        void* indexData = indexBuffer->acquire(3 * numTriangles, true);
+
+        extractor.populateIndexBuffer(indexData, 3 * numTriangles, itemInfo.indexDesc);
+        indexBuffer->commit(indexData);
+
+        MIndexBuffer* rawIndexBuffer = indexBuffer.get();
+        indexBuffers.push_back(std::move(indexBuffer));
+        
+        return rawIndexBuffer;
+    }
+
+    MRenderItem* createRenderItem(MSubSceneContainer& container, const RenderItemInfo& itemInfo) {
+        MRenderItem* renderItem = container.find(itemInfo.renderItemName);
+        if (renderItem) return renderItem;
+
+        renderItem = MRenderItem::Create(itemInfo.renderItemName, MRenderItem::MaterialSceneItem, MGeometry::kTriangles);
+        renderItem->setDrawMode(static_cast<MGeometry::DrawMode>(MGeometry::kShaded | MGeometry::kTextured));
+        renderItem->setSelectionMask(MSelectionMask::kSelectMeshes);
+        renderItem->setWantConsolidation(true);
+        renderItem->setShader(itemInfo.shaderInstance);
+        container.add(renderItem);
+
+        return renderItem;
+    }
+
+    /**
+     * This method is responsible for populating the MSubSceneContainer with render items. In our case, we want the our custom VoxelShape
+     * to have the same geometry, topology, and shading as the original mesh it deforms. To do so, we use the shading sets of the original mesh
+     * to tell us what geometry requirements we need to extract and recreate here.
+     */
+    void update(MSubSceneContainer& container, const MFrameContext& frameContext) override
+    {
+        if (!voxelShape) return;
+        MStatus status;
+
+        const MDagPath originalGeomPath = voxelShape->pathToOriginalGeometry();
+        MFnMesh originalMeshFn(originalGeomPath.node());
+        if (originalMeshFn.numVertices() == 0) return;
+        
+        // Get all shaders from the original mesh. It will tell us the required vertex buffers,
+        // and its mapping of faces to shaders will tell us how to create index buffers and render items.
+        MObjectArray shadingSets;
+        MIntArray faceIdxToShader;
+        status = originalMeshFn.getConnectedShaders(originalGeomPath.instanceNumber(), shadingSets, faceIdxToShader);
+        if (status != MStatus::kSuccess) return;
+        MObjectArray shadingSetFaceComponents = getShadingSetFaceComponents(shadingSets, faceIdxToShader);
+
+        // Extract the geometry requirements (vertex and index buffer descriptorss) from the shaders.
+        // Then use MGeometryExtractor to extract the vertex and index buffers from the original mesh.
+        MGeometryRequirements geomReqs;
+        std::vector<RenderItemInfo> renderItemInfos;
+        renderItemInfos.reserve(shadingSets.length());
+        buildGeometryRequirements(shadingSets, shadingSetFaceComponents, originalGeomPath, geomReqs, renderItemInfos);
+        MGeometryExtractor extractor(geomReqs, originalGeomPath, kPolyGeom_Normal, &status);
+        if (status != MStatus::kSuccess) return;
+
+        MVertexBufferArray vertexBufferArray;        
+        const unsigned int vertexCount = extractor.vertexCount(); 
+        const MVertexBufferDescriptorList& vbDescList = geomReqs.vertexRequirements();
+        for (int i = 0; i < vbDescList.length(); ++i) {
+            MVertexBufferDescriptor vbDesc;
+            if (!vbDescList.getDescriptor(i, vbDesc)) continue;
+
+            createVertexBuffer(vbDesc, extractor, vertexCount, vertexBufferArray);
+        }
+
+        // Create an index buffer + render item for each shading set of the original mesh (which corresponds to an indexing requirement)
+        const MBoundingBox bounds = voxelShape->boundingBox();
+        for (const RenderItemInfo& itemInfo : renderItemInfos) {
+            MIndexBuffer* rawIndexBuffer = createIndexBuffer(itemInfo, extractor);
+            if (!rawIndexBuffer) continue;
+
+            MRenderItem* renderItem = createRenderItem(container, itemInfo);
+            status = setGeometryForRenderItem(*renderItem, vertexBufferArray, *rawIndexBuffer, &bounds);
+        }
+    }
+
+private:
+    VoxelShape* voxelShape;
+    std::vector<unique_ptr<MVertexBuffer>> vertexBuffers;
+    std::vector<unique_ptr<MIndexBuffer>> indexBuffers;
+
+    VoxelSubSceneOverride(const MObject& obj)
+    : MPxSubSceneOverride(obj) {
+        MFnDependencyNode dn(obj);
+        voxelShape = static_cast<VoxelShape*>(dn.userNode());
+    }
+};
