bac5410fb9479ef55cf06189194f60764b043824
Matt Schwartz <mzschwartz5@gmail.com>
2025-09-09 02:07:27 -0400

    First working implementation of subscene override deformer!

--- shaders/deformvertices.hlsl
+++ shaders/deformvertices.hlsl
@@ -18,6 +18,18 @@ cbuffer DeformConstants : register(b0)
     uint padding2;
 };
 
+/**
+ * Each thread represents a vertex. Based on the voxel the vertex belongs to, we deform the vertex
+ * according to the positions of the particles in that voxel, compared to their original positions.
+ * Similarly, we deform the normals.
+ * 
+ * A couple notes:
+ * 1. Vertices and normals may be duplicated in the input buffers if a vertex is shared between triangles
+ *    and has different normals or UVs for each triangle. This is fine, we just do a little redundant work.
+ * 2. Maya's GPU deformer API abstracts this duplicate vertex concern away - you only deform the logical vertices.
+ *    However, the deformer API gives *no* way to control normals. It calculates them automatically. For most workflows,
+ *    that's probably fine. The discontinuities between destructible voxels, however, make Maya's auto-calculated normals look bad.
+ */
 [numthreads(DEFORM_VERTICES_THREADS, 1, 1)]
 void main(uint3 gId : SV_DispatchThreadID) 
 {
@@ -33,10 +45,7 @@ void main(uint3 gId : SV_DispatchThreadID)
 
     // Note: originalParticles contains only one reference particle per voxel, thus the index into it
     // does not need to be multiplied by 8 to account for the 8 particles per voxel.
-    float3 v0_orig = originalParticlePositions[voxelId + 0].xyz;
-    float3 v1_orig = originalParticlePositions[voxelId + 1].xyz;
-    float3 v2_orig = originalParticlePositions[voxelId + 2].xyz;
-    float3 v3_orig = originalParticlePositions[voxelId + 4].xyz;
+    float3 v0_orig = originalParticlePositions[voxelId].xyz;
 
     float3 e0 = normalize(v1 - v0);
     float3 e1 = normalize(v2 - v0);
@@ -48,7 +57,9 @@ void main(uint3 gId : SV_DispatchThreadID)
                             + restPosition.z * e2;
 
     uint outOffset = gId.x * 12; // 3 floats * 4 bytes each
-    outVertPositions.Store3(outOffset, deformedPos);
+    outVertPositions.Store3(outOffset, asuint(deformedPos));
 
     // TODO: normals
+    float3 origNormal = originalVertNormals[gId.x];
+    outVertNormals.Store3(outOffset, asuint(origNormal));
 }
