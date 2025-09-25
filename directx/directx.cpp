9a5511834dc4045a1a7e16c98daf9db4f1110bd6
Matt Schwartz <mzschwartz5@gmail.com>
2025-09-25 01:52:09 -0400

    Defines static methods in directx class for buffers and views

--- directx/directx.cpp
+++ directx/directx.cpp
@@ -48,3 +48,65 @@ HINSTANCE DirectX::getPluginInstance()
 {
     return pluginInstance;
 }
+
+ComPtr<ID3D11ShaderResourceView> DirectX::createSRV(
+    ComPtr<ID3D11Buffer>& buffer,
+    UINT elementCount,
+    UINT offset,
+    bool rawBuffer
+) {
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+    D3D11_BUFFER_DESC bufferDesc = {};
+    int numElements = elementCount;
+    if (numElements == 0) {
+        buffer->GetDesc(&bufferDesc);
+        numElements = rawBuffer ?  bufferDesc.ByteWidth / 4 : 
+                                   bufferDesc.ByteWidth / bufferDesc.StructureByteStride;
+    }
+
+    if (rawBuffer) {
+        srvDesc.Format = DXGI_FORMAT_R32_TYPELESS;
+        srvDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+    } else {
+        srvDesc.Format = DXGI_FORMAT_UNKNOWN;
+        srvDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+    }
+    srvDesc.Buffer.FirstElement = offset;
+    srvDesc.Buffer.NumElements = numElements;
+
+    ComPtr<ID3D11ShaderResourceView> srv;
+    HRESULT hr = dxDevice->CreateShaderResourceView(buffer.Get(), &srvDesc, srv.GetAddressOf());
+    return srv;
+
+}
+
+ComPtr<ID3D11UnorderedAccessView> DirectX::createUAV(
+    ComPtr<ID3D11Buffer>& buffer,
+    UINT elementCount,
+    UINT offset,
+    bool rawBuffer
+) {
+    D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+    D3D11_BUFFER_DESC bufferDesc = {};
+    int numElements = elementCount;
+    if (numElements == 0) {
+        buffer->GetDesc(&bufferDesc);
+        numElements = rawBuffer ?  bufferDesc.ByteWidth / 4 : 
+                                   bufferDesc.ByteWidth / bufferDesc.StructureByteStride;
+    }
+
+    if (rawBuffer) {
+        uavDesc.Format = DXGI_FORMAT_R32_TYPELESS;
+        uavDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+        uavDesc.Buffer.Flags = D3D11_BUFFER_UAV_FLAG_RAW;
+    } else {
+        uavDesc.Format = DXGI_FORMAT_UNKNOWN;
+        uavDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+    }
+    uavDesc.Buffer.FirstElement = offset;
+    uavDesc.Buffer.NumElements = numElements;
+
+    ComPtr<ID3D11UnorderedAccessView> uav;
+    HRESULT hr = dxDevice->CreateUnorderedAccessView(buffer.Get(), &uavDesc, uav.GetAddressOf());
+    return uav;
+}
--- directx/directx.h
+++ directx/directx.h
@@ -2,8 +2,11 @@
 #include <maya/MGlobal.h>
 #include <d3d11.h>
 #include <d3dcompiler.h>
+#include <vector>
 #include <maya/MViewport2Renderer.h>
+#include <wrl/client.h>
 using namespace MHWRender;
+using Microsoft::WRL::ComPtr;
 
 class DirectX
 {
@@ -16,6 +19,95 @@ public:
     static ID3D11Device* getDevice();
     static ID3D11DeviceContext* getContext();
     static HINSTANCE getPluginInstance();
+
+    template<typename T>
+    static ComPtr<ID3D11Buffer> createReadOnlyBuffer(
+        std::vector<T>& data,
+        UINT additionalBindFlags = 0,
+        bool rawBuffer = false
+    ) {
+        D3D11_BUFFER_DESC bufferDesc = {};
+
+        bufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+        bufferDesc.ByteWidth = static_cast<UINT>(sizeof(T) * data.size());
+        bufferDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | additionalBindFlags;
+        bufferDesc.CPUAccessFlags = 0;
+        if (rawBuffer) {
+            bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS;
+        }
+        else {
+            bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+            bufferDesc.StructureByteStride = sizeof(T);
+        }
+
+        D3D11_SUBRESOURCE_DATA initData = {};
+        initData.pSysMem = data.data();
+
+        ComPtr<ID3D11Buffer> buffer;
+        HRESULT hr = dxDevice->CreateBuffer(&bufferDesc, &initData, &buffer);
+        return buffer;   
+    }
+
+    template<typename T>
+    static ComPtr<ID3D11Buffer> createReadWriteBuffer(
+        std::vector<T>& data,
+        UINT additionalBindFlags = 0,
+        bool rawBuffer = false
+    ) {
+        D3D11_BUFFER_DESC bufferDesc = {};
+
+        bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        bufferDesc.ByteWidth = static_cast<UINT>(sizeof(T) * data.size());
+        bufferDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS | additionalBindFlags;
+        bufferDesc.CPUAccessFlags = 0;
+
+        if (rawBuffer) {
+            bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS;
+        }
+        else {
+            bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+            bufferDesc.StructureByteStride = sizeof(T);
+        }
+
+        D3D11_SUBRESOURCE_DATA initData = {};
+        initData.pSysMem = data.data();
+
+        ComPtr<ID3D11Buffer> buffer;
+        HRESULT hr = dxDevice->CreateBuffer(&bufferDesc, &initData, &buffer);
+        return buffer;   
+    }
+    
+    template<typename T>
+    static ComPtr<ID3D11Buffer> createConstantBuffer(const T& data) {
+        D3D11_BUFFER_DESC bufferDesc = {};
+
+        bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+        bufferDesc.ByteWidth = static_cast<UINT>(sizeof(T));
+        bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+        bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+        bufferDesc.MiscFlags = 0;
+
+        D3D11_SUBRESOURCE_DATA initData = {};
+        initData.pSysMem = &data;
+
+        ComPtr<ID3D11Buffer> buffer;
+        HRESULT hr = dxDevice->CreateBuffer(&bufferDesc, &initData, &buffer);
+        return buffer;   
+    }
+
+    static ComPtr<ID3D11ShaderResourceView> createSRV(
+        ComPtr<ID3D11Buffer>& buffer,
+        UINT elementCount = 0,
+        UINT offset = 0,
+        bool rawBuffer = false
+    );
+
+    static ComPtr<ID3D11UnorderedAccessView> createUAV(
+        ComPtr<ID3D11Buffer>& buffer,
+        UINT elementCount = 0,
+        UINT offset = 0,
+        bool rawBuffer = false
+    );
 
 private:
     static HINSTANCE pluginInstance;
