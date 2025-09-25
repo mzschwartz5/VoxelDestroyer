f82c334dcc37f73b294e522a5d69352e0ba71187
Matt Schwartz <mzschwartz5@gmail.com>
2025-09-25 15:06:28 -0400

    Moves more utility functions from computeshader to directx

--- directx/compute/computeshader.h
+++ directx/compute/computeshader.h
@@ -37,30 +37,6 @@ protected:
     virtual void bind() = 0;
     virtual void unbind() = 0;
 
-    /**
-     * Generic method to update a constant buffer with new data.
-     */
-    template<typename T>
-    void updateConstantBuffer(const ComPtr<ID3D11Buffer>& buffer, const T& data) {
-        D3D11_MAPPED_SUBRESOURCE mappedResource;
-        HRESULT hr = DirectX::getContext()->Map(buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
-        if (SUCCEEDED(hr)) {
-            memcpy(mappedResource.pData, &data, sizeof(T));
-            DirectX::getContext()->Unmap(buffer.Get(), 0);
-        } else {
-            MGlobal::displayError("Failed to map constant buffer.");
-        }
-    }
-
-    /*
-    * Clears a UINT buffer with the value 0.
-    */
-    void clearUintBuffer(const ComPtr<ID3D11UnorderedAccessView>& uav) {
-        // See docs: 4 values are required even though only the first will be used, in our case.
-        UINT clearValues[4] = { 0, 0, 0, 0 };
-        DirectX::getContext()->ClearUnorderedAccessViewUint(uav.Get(), clearValues);
-    }
-
     void load() {
         if (shaderCache.find(id) != shaderCache.end()) {
             shaderPtr = shaderCache.at(id);
