649e659571af877be2d389bfe70594e89b07d304
Matt Schwartz <mzschwartz5@gmail.com>
2025-10-13 19:27:26 -0400

    Adds output render target to paint operation

--- custommayaconstructs/draw/voxelpaintrenderoperation.h
+++ custommayaconstructs/draw/voxelpaintrenderoperation.h
@@ -15,4 +15,53 @@ public:
         return MStatus::kSuccess;
     }
 
+    MRenderTarget* const* targetOverrideList(unsigned int &listSize) override {
+        unsigned int viewportWidth, viewportHeight;
+        MRenderer::theRenderer()->outputTargetSize(viewportWidth, viewportHeight);
+        createOrUpdateRenderTargets(viewportWidth, viewportHeight);
+
+        listSize = 1;
+        return &paintOutputRenderTarget;
+    }
+
+    void createOrUpdateRenderTargets(unsigned int targetWidth, unsigned int targetHeight) {
+        MRenderer* renderer = MRenderer::theRenderer();
+        if (!renderer) return;
+
+        const MRenderTargetManager* targetManager = renderer->getRenderTargetManager();
+        if (!targetManager) return;
+        if (targetWidth == currentOutputWidth && targetHeight == currentOutputHeight) return;
+
+        currentOutputWidth = targetWidth;
+        currentOutputHeight = targetHeight;
+
+        MRenderTargetDescription desc(
+            paintOutputRenderTargetName,
+            currentOutputWidth, currentOutputHeight,
+            1, // number of samples
+            MHWRender::kR32_UINT, // format
+            1, // array slices
+            false // is cube map
+        );
+
+        if (paintOutputRenderTarget) {
+            paintOutputRenderTarget->updateDescription(desc);
+            return;
+        }
+
+        paintOutputRenderTarget = targetManager->acquireRenderTarget(desc);
+    }
+
+    void clearRenderTargets() {
+        if (paintOutputRenderTarget) {
+            MRenderer::theRenderer()->getRenderTargetManager()->releaseRenderTarget(paintOutputRenderTarget);
+            paintOutputRenderTarget = nullptr;
+        }
+    }
+
+private:
+    inline static const MString paintOutputRenderTargetName = "voxelPaintOutputTarget";
+    MRenderTarget* paintOutputRenderTarget = nullptr;
+    unsigned int currentOutputWidth = 0;
+    unsigned int currentOutputHeight = 0;
 };
