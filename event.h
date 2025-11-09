58d7dc6b23f5a1d23541785d48844804c9212503
Matt Schwartz <mzschwartz5@gmail.com>
2025-11-08 23:03:04 -0500

    Makes event class more robust

--- event.h
+++ event.h
@@ -6,7 +6,26 @@
 
 class EventBase {
 public:
-    using Unsubscribe = std::function<void()>;
+    // Wrapper around std::function<void()> that is safe to call even if default-constructed,
+    // and safe to call multiple times.
+    class Unsubscribe {
+    public:
+        Unsubscribe() noexcept = default;
+        Unsubscribe(std::function<void()> f) : fn(std::move(f)) {}
+
+        void operator()() noexcept {
+            if (!fn) return;
+
+            auto f = std::move(fn); // move out so call only works once.
+            f();                    // invoke once
+        }
+
+        bool has_value() const noexcept { return static_cast<bool>(fn); }
+        explicit operator bool() const noexcept { return has_value(); }
+
+    private:
+        mutable std::function<void()> fn;
+    };
 };
 
 /**
