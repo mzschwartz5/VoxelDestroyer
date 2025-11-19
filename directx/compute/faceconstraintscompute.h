ff8421cf29b68519f2014078e018572b1f45a8b6
Matt Schwartz <mzschwartz5@gmail.com>
2025-11-19 01:05:21 -0500

    Wires up paint values to face constraint sim values (WIP)

--- directx/compute/faceconstraintscompute.h
+++ directx/compute/faceconstraintscompute.h
@@ -7,16 +7,16 @@ struct FaceConstraintsCB {
 	std::array<uint, 4> faceOneIndices;
 	std::array<uint, 4> faceTwoIndices;
     uint numContraints;
+    int faceOneId;
+    int faceTwoId;
     int padding0;
-    int padding1;
-    int padding2;
 };
 
 struct FaceConstraint {
 	int voxelOneIdx;
 	int voxelTwoIdx;
-	float tensionLimit;
-	float compressionLimit;
+	float tensionLimit = 0.0f;
+	float compressionLimit = 0.0f;
 };
 
 class FaceConstraintsCompute : public ComputeShader
@@ -29,6 +29,8 @@ public:
         const ComPtr<ID3D11Buffer>& voxelSimInfoBuffer
     ) : ComputeShader(IDR_SHADER4), voxelSimInfoBuffer(voxelSimInfoBuffer)
     {
+        // This shader has a second entry point for updating constraint limits from paint data.
+        loadEntryPoint(updateFaceConstraintsEntryPoint);
         initializeBuffers(constraints);
     };
 
@@ -40,12 +42,17 @@ public:
 
     void dispatch() override
     {
-        ComputeShader::dispatch(numWorkgroups[activeConstraintAxis]);
+        for (activeConstraintAxis = 0; activeConstraintAxis < 3; activeConstraintAxis++) {
+            ComputeShader::dispatch(numWorkgroups[activeConstraintAxis]);
+        }
     };
 
-	void updateActiveConstraintAxis(int axis) {
-		activeConstraintAxis = axis;
-	}
+    void updateFaceConstraintsFromPaint(const ComPtr<ID3D11UnorderedAccessView>& paintDeltaUAV) {
+        this->paintDeltaUAV = paintDeltaUAV;
+        for (activeConstraintAxis = 0; activeConstraintAxis < 3; activeConstraintAxis++) {
+            ComputeShader::dispatch(numWorkgroups[activeConstraintAxis], updateFaceConstraintsEntryPoint);
+        }
+    }
 
     void setPositionsUAV(const ComPtr<ID3D11UnorderedAccessView>& positionsUAV) {
         this->positionsUAV = positionsUAV;
@@ -56,6 +63,7 @@ public:
     }
 
 private:
+    inline static const std::string updateFaceConstraintsEntryPoint = "updateFaceConstraintsFromPaint";
     int activeConstraintAxis = 0; // x = 0, y = 1, z = 2
     std::array<int, 3> numWorkgroups = { 0, 0, 0 };
     std::array<ComPtr<ID3D11UnorderedAccessView>, 3> faceConstraintUAVs;
@@ -64,10 +72,11 @@ private:
     ComPtr<ID3D11Buffer> voxelSimInfoBuffer;
     ComPtr<ID3D11UnorderedAccessView> isSurfaceUAV;
     ComPtr<ID3D11UnorderedAccessView> positionsUAV;
+    ComPtr<ID3D11UnorderedAccessView> paintDeltaUAV; // Only used during update from paint values
 
     void bind() override
     {
-        ID3D11UnorderedAccessView* uavs[] = { positionsUAV.Get(), faceConstraintUAVs[activeConstraintAxis].Get(), isSurfaceUAV.Get(), facePaintUAV.Get() };
+        ID3D11UnorderedAccessView* uavs[] = { positionsUAV.Get(), faceConstraintUAVs[activeConstraintAxis].Get(), isSurfaceUAV.Get(), paintDeltaUAV.Get() };
         DirectX::getContext()->CSSetUnorderedAccessViews(0, ARRAYSIZE(uavs), uavs, nullptr);
 
         ID3D11Buffer* cbvs[] = { voxelSimInfoBuffer.Get(), faceConstraintsCBs[activeConstraintAxis].Get() };
@@ -76,7 +85,7 @@ private:
 
     void unbind() override
     {
-        ID3D11UnorderedAccessView* uavs[] = { nullptr, nullptr, nullptr };
+        ID3D11UnorderedAccessView* uavs[] = { nullptr, nullptr, nullptr, nullptr };
         DirectX::getContext()->CSSetUnorderedAccessViews(0, ARRAYSIZE(uavs), uavs, nullptr);
 
         ID3D11Buffer* cbvs[] = { nullptr, nullptr };
@@ -85,10 +94,11 @@ private:
     
     void initializeBuffers(const std::array<std::vector<FaceConstraint>, 3>& constraints) {
 
+        // Order of vertex indices and face IDs corresponds to definitions in cube.h
         std::array<FaceConstraintsCB, 3> faceConstraintsCBData = {
-            FaceConstraintsCB{{1, 3, 5, 7}, {0, 2, 4, 6}, static_cast<uint>(constraints[0].size()), 0, 0, 0},
-            FaceConstraintsCB{{2, 3, 6, 7}, {0, 1, 4, 5}, static_cast<uint>(constraints[1].size()), 0, 0, 0},
-            FaceConstraintsCB{{4, 5, 6, 7}, {0, 1, 2, 3}, static_cast<uint>(constraints[2].size()), 0, 0, 0}
+            FaceConstraintsCB{{1, 3, 5, 7}, {0, 2, 4, 6}, static_cast<uint>(constraints[0].size()), 0, 3, 0},
+            FaceConstraintsCB{{2, 3, 6, 7}, {0, 1, 4, 5}, static_cast<uint>(constraints[1].size()), 1, 4, 0},
+            FaceConstraintsCB{{4, 5, 6, 7}, {0, 1, 2, 3}, static_cast<uint>(constraints[2].size()), 2, 5, 0}
         };        
 
         for (int i = 0; i < 3; i++) {
--- shaders/faceconstraints.hlsl
+++ shaders/faceconstraints.hlsl
@@ -22,12 +22,12 @@ cbuffer VoxelSimBuffer : register(b0)
 
 cbuffer FaceConstraintsCB : register(b1)
 {
-    uint4 faceAParticles;
-    uint4 faceBParticles;
+    uint4 faceAParticles; // Which particles of voxel A are involved in the face constraint
+    uint4 faceBParticles; // Which particles of voxel B are involved in the face constraint
     uint numConstraints;
+    int faceAId;          // Which face index this constraint corresponds to on voxel A (only used for paint value lookup)
+    int faceBId;          // Which face index this constraint corresponds to on voxel B (only used for paint value lookup)
     int padding0;
-    int padding1;
-    int padding2;
 };
 
 RWStructuredBuffer<float4> positions : register(u0);
@@ -48,9 +48,7 @@ void breakConstraint(int constraintIdx, int voxelAIdx, int voxelBIdx) {
 */
 [numthreads(VGS_THREADS, 1, 1)]
 void main(
-    uint3 globalThreadId : SV_DispatchThreadID,
-    uint3 groupId : SV_GroupID,
-    uint3 localThreadId : SV_GroupThreadID
+    uint3 globalThreadId : SV_DispatchThreadID
 )
 {
     uint constraintIdx = globalThreadId.x;
@@ -108,4 +106,48 @@ void main(
             positions[voxelBParticlesIdx + faceBParticles[j]] = pos[faceAParticles[j]];
         }
     }
+}
+
+RWBuffer<float> paintDeltas : register(u3);
+
+// This entry point is for updating the face constraints based on paint values.
+// One thread per face constraint (over three dispatches, one per axis - just because that's how the face constraint data is stored).
+// This runs every time a brush stroke ends - that way, we can use the computed delta to know which faces were updated, and can 
+// update the neighboring face in the constraint pair. (And it also means the simulation values are always in sync).
+[numthreads(VGS_THREADS, 1, 1)]
+void updateFaceConstraintsFromPaint(
+    uint3 globalThreadId : SV_DispatchThreadID
+)
+{
+    uint constraintIdx = globalThreadId.x;
+    if (constraintIdx >= numConstraints) return;
+
+    FaceConstraint constraint;
+    constraint = faceConstraints[constraintIdx];
+
+    int voxelAIdx = constraint.voxelAIdx;
+    int voxelBIdx = constraint.voxelBIdx;
+    if (voxelAIdx == -1 || voxelBIdx == -1) return;
+
+    int paintValueAIdx = voxelAIdx * 6 + faceAId;
+    int paintValueBIdx = voxelBIdx * 6 + faceBId;
+
+    // In theory, in a given brush stroke, the user can only paint one side of a face constraint. (If camera-based painting is off,
+    // they can get both sides, but they would be the same value). So we take whichever value is non-zero (if either), and set the constraint limits
+    // based on that. And also set the other paint value in the pair to the same value so both sides are visually painted consistent.
+    float paintDeltaA = paintDeltas[paintValueAIdx];
+    float paintDeltaB = paintDeltas[paintValueBIdx];
+    if (paintDeltaA < eps && paintDeltaB < eps) return;
+
+    // Branch-free selection of the paint delta to use (pick B if non-zero, else A)
+    float useB = abs(sign(paintDeltaB));
+    float selectedDelta = paintDeltaA + useB * (paintDeltaB - paintDeltaA);
+    int unselectedIdx = paintValueBIdx + (int)(useB) * (paintValueAIdx - paintValueBIdx);
+
+    paintDeltas[unselectedIdx] = selectedDelta;
+
+    // For now, same paint value for both limits
+    constraint.tensionLimit = selectedDelta;
+    constraint.compressionLimit = -selectedDelta;
+    faceConstraints[constraintIdx] = constraint;
 }
