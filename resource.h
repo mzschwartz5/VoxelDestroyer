ea18e4b4476315cffdfcf0dd36cf37c51e15aa01
Matt Schwartz <mzschwartz5@gmail.com>
2025-12-05 15:04:29 -0500

    More robust shelf tab deletion on plugin unload

--- resource.h
+++ resource.h
@@ -23,17 +23,18 @@
 #define IDR_MEL3                        121
 #define IDR_MEL4                        122
 #define IDR_MEL5                        123
-#define IDR_PNG_VOXELIZER               124
-#define IDR_PNG_VOXELDRAG               125
-#define IDR_PNG_VOXELCOLLIDER           126
-#define IDR_PNG_VOXELPAINT              127
+#define IDR_MEL6                        124
+#define IDR_PNG_VOXELIZER               125
+#define IDR_PNG_VOXELDRAG               126
+#define IDR_PNG_VOXELCOLLIDER           127
+#define IDR_PNG_VOXELPAINT              128
 
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        128
+#define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
