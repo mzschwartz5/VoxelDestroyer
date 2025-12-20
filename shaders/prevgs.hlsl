dc8a556b8d4d8072d3ca16e44763e1f1b4899196
Matt Schwartz <mzschwartz5@gmail.com>
2025-12-20 10:07:46 -0500

    Split out entry points into own shaders (pt. 2)

--- shaders/prevgs.hlsl
+++ shaders/prevgs.hlsl
@@ -1,21 +1,6 @@
 #include "common.hlsl"
 #include "constants.hlsli"
-
-StructuredBuffer<bool> isDragging : register(t0);
-RWStructuredBuffer<float4> positions : register(u0);
-RWStructuredBuffer<float4> oldPositions : register(u1);
-
-cbuffer VoxelSimBuffer : register(b0)
-{
-    float GRAVITY_STRENGTH;
-    float GROUND_Y;
-    float TIMESTEP;
-    uint numParticles;
-    float massLow;
-    float massHigh;
-    int padding0;
-    int padding1;
-};
+#include "prevgs_shared.hlsl"
 
 [numthreads(VGS_THREADS, 1, 1)]
 void main(uint3 gId : SV_DispatchThreadID) 
@@ -43,32 +28,3 @@ void main(uint3 gId : SV_DispatchThreadID)
     positions[gId.x] = pos;
 }
 
-RWBuffer<float> paintDeltas : register(u2);
-RWBuffer<float> paintValues : register(u3);
-
-// Second entry point in this shader, used for updating particle (inverse) masses based on paint values
-// Users paint particle _mass_, so we also need to convert to inverse mass here, 
-// treating negative paint values (indicating infinite mass) as 0 inverse mass (the convention for infinite mass in the simulation)
-// One thread per particle.
-[numthreads(VGS_THREADS, 1, 1)]
-void updateParticleMassFromPaint(uint3 gId : SV_DispatchThreadID) {
-    if (gId.x >= numParticles) return;
-
-    float paintDelta = paintDeltas[gId.x];
-    if (abs(paintDelta) < 1e-6) return;
-
-    float paintValue = paintValues[gId.x];
-    float packedRadiusAndInvMass = positions[gId.x].w;
-
-    if (paintValue < 0.0f) { 
-        // Infinite mass case.
-        packedRadiusAndInvMass = updateMass(packedRadiusAndInvMass, 0.0f);
-    } else {
-        // Note: we prevent the user from setting a mass lower limit of 0, so no need to check for division by zero here.
-        float mass = lerp(massLow, massHigh, paintValue);
-        float invMass = 1.0f / mass;
-        packedRadiusAndInvMass = updateMass(packedRadiusAndInvMass, invMass);
-    }
-
-    positions[gId.x].w = packedRadiusAndInvMass;
-}
---
+++ shaders/updateParticleMassFromPaint.hlsl
@@ -0,0 +1,30 @@
+#include "constants.hlsli"
+#include "common.hlsl"
+#include "prevgs_shared.hlsl"
+
+// Used for updating particle (inverse) masses based on paint values
+// Users paint particle _mass_, so we also need to convert to inverse mass here, 
+// treating negative paint values (indicating infinite mass) as 0 inverse mass (the convention for infinite mass in the simulation)
+// One thread per particle.
+[numthreads(VGS_THREADS, 1, 1)]
+void main(uint3 gId : SV_DispatchThreadID) {
+    if (gId.x >= numParticles) return;
+
+    float paintDelta = paintDeltas[gId.x];
+    if (abs(paintDelta) < eps) return;
+
+    float paintValue = paintValues[gId.x];
+    float packedRadiusAndInvMass = positions[gId.x].w;
+
+    if (paintValue < 0.0f) { 
+        // Infinite mass case.
+        packedRadiusAndInvMass = updateMass(packedRadiusAndInvMass, 0.0f);
+    } else {
+        // Note: we prevent the user from setting a mass lower limit of 0, so no need to check for division by zero here.
+        float mass = lerp(massLow, massHigh, paintValue);
+        float invMass = 1.0f / mass;
+        packedRadiusAndInvMass = updateMass(packedRadiusAndInvMass, invMass);
+    }
+
+    positions[gId.x].w = packedRadiusAndInvMass;
+}
