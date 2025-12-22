38fa3a3b2277dcfe9ccbd43731a1a3bf02012313
Matt Schwartz <mzschwartz5@gmail.com>
2025-12-21 20:33:41 -0500

    small optimization to prevgs step

--- shaders/prevgs.hlsl
+++ shaders/prevgs.hlsl
@@ -12,16 +12,12 @@ void main(uint3 gId : SV_DispatchThreadID)
     float4 oldPos = oldPositions[gId.x];
     oldPositions[gId.x] = pos;
 
-    // Note: delay applying timestep (i.e. don't calculate velocity now) because:
-    // 1. Division is expensive
-    // 2. Makes delta rounding errors worse when timestep is small
-    float4 delta = (pos - oldPos);
-    
     int voxelIndex = gId.x >> 3;
-    if (!isDragging[voxelIndex]) {
-        delta += float4(0, preVgsConstants.gravityStrength, 0, 0) * preVgsConstants.timeStep * preVgsConstants.timeStep;
-        pos.xyz += delta.xyz;
-    }
+    if (isDragging[voxelIndex]) return;
+
+    float4 delta = (pos - oldPos);
+    delta += float4(0, preVgsConstants.gravityStrength, 0, 0) * preVgsConstants.timeStep * preVgsConstants.timeStep;
+    pos.xyz += delta.xyz;
     
     // Write back to global memory
     positions[gId.x] = pos;
